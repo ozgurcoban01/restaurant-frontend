b19e5e578e4f0a18ff5dabddf5ecec3d10bab7d9
Özgür Çoban <cobanozgur55@gmail.com>
2023-08-31 18:47:35 +0300

    qr scan almost done

--- src/components/consumerComponents/ScanQR.jsx
+++ src/components/consumerComponents/ScanQR.jsx
@@ -3,10 +3,12 @@ import { Html5QrcodeScanner } from 'html5-qrcode'
 import { Box, Container } from '@mui/material'
 import { lime, purple,red,green,orange,deepOrange,deepPurple } from '@mui/material/colors';
 import "../../index.css";
+import CircularProgress from '@mui/material/CircularProgress';
 const ScanQR = () => {
-    const [scanResult,setScanResukt]=useState(null)
+    
+    const [scanResult,setScanResult]=useState(null)
+    const [loading,setLoading]=useState(false)
      useEffect(()=>{
-
         const scanner=new Html5QrcodeScanner('reader',{
             qrbox:{
                 width:250,
@@ -16,33 +18,50 @@ const ScanQR = () => {
         })
       
         const success=(result)=>{
-            scanner.clear()
-            setScanResukt(result)
+            setLoading(true)
+            setTimeout(()=>{
+                setLoading(false)
+                scanner.clear()
+                setScanResult(result)
+            },2000)
+
+       
         }
         const error=(err)=>{
             console.warn(err)
+            
         }
         scanner.render(success,error)
+        
+    if(!scanResult){
         const button=document.querySelector("#html5-qrcode-button-camera-permission")
         const frame=document.querySelector("#reader")
-  
-        button.style["background-color"]=deepPurple[300]
+        const scanFile=document.querySelector("#html5-qrcode-anchor-scan-type-change")
+       
+        scanFile.style["display"]="none"
+        scanFile.innerHTML=""
+        button.style["background-color"]=deepPurple[600]
         button.style["border"]="none"
         button.style["padding"]="10px"
         button.style["border-radius"]="10px"
         button.style["cursor"]="pointer"
-      
 
         frame.style["border"]="none"
+       }
     },[])
-    
+    let scanResultDiv;
+   
+    if (scanResult) {
+        scanResultDiv = <div >{scanResult}</div>;
+      } else {
+        scanResultDiv = <div id="reader" ></div> ;
+      }
+      
   return (
     <div>
         <Box sx={{backgroundColor:deepPurple[100]}}>
             <Container sx={{display:"flex",height:"100vh",alignItems:"center",justifyContent:"center"}}>
-            {
-            scanResult?<div>{scanResult}</div>:<div id="reader" ></div> 
-        }
+            {loading?<CircularProgress />:scanResultDiv}
             </Container>
         </Box>
         
